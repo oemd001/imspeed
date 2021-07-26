2bf5fc9282a2d8bcee58c5d2fe42d0df7621d518
oemd001 <kennethchen2001@gmail.com>
2021-07-26 07:39:52 -0700

    Updated problem 67

--- java/easy/67_add-binary.java
+++ java/easy/67_add-binary.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder(); 
@@ -18,6 +19,38 @@ class Solution {
             
             i--;
             j--;
+        }
+        if (carry != 0) {
+            sb.append(carry);
+            return sb.reverse().toString(); 
+        }
+        return sb.reverse().toString(); 
+    }
+}
+*/
+
+//basically a copied solution, but I did this myself
+class Solution {
+    public String addBinary(String a, String b) {
+        int a_length = a.length() - 1; 
+        int b_length = b.length() - 1;
+        StringBuilder sb = new StringBuilder(); 
+        int carry = 0;
+        
+        while (a_length >= 0 || b_length >=0 ) {
+            int sum = carry; 
+            if (a_length >= 0) {
+                sum = sum + a.charAt(a_length) - '0';
+            }
+            if (b_length >= 0) {
+                sum = sum + b.charAt(b_length) - '0';
+            }
+            
+            sb.append(sum % 2);
+            carry = sum / 2;
+            
+            a_length--;
+            b_length--;
         }
         if (carry != 0) {
             sb.append(carry);
