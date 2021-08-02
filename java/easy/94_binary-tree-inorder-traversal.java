5b328636f50265635d3c7cf102a984bef82b1380
oemd001 <kennethchen2001@gmail.com>
2021-08-01 23:13:09 -0700

    Added recursive funciton on 94

--- java/easy/94_binary-tree-inorder-traversal.java
+++ java/easy/94_binary-tree-inorder-traversal.java
@@ -41,28 +41,23 @@ class Solution {
         return output_arr;
     }
 }
-/*
+//recursion methodology
 class Solution {
     public List < Integer > inorderTraversal(TreeNode root) {
         List < Integer > res = new ArrayList < > ();
-        TreeNode curr = root;
-        TreeNode pre;
-        while (curr != null) {
-            if (curr.left == null) {
-                res.add(curr.val);
-                curr = curr.right; // move to next right node
-            } else { // has a left subtree
-                pre = curr.left;
-                while (pre.right != null) { // find rightmost
-                    pre = pre.right;
-                }
-                pre.right = curr; // put cur after the pre node
-                TreeNode temp = curr; // store cur node
-                curr = curr.left; // move cur to the top of the new tree
-                temp.left = null; // original cur left be null, avoid infinite loops
-            }
-        }
+        helper(root, res);
         return res;
     }
-    */
+
+    public void helper(TreeNode root, List < Integer > res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
 }
