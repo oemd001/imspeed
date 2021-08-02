007a3370bf5136d2c0968ab99552bc33c3d336f4
oemd001 <kennethchen2001@gmail.com>
2021-08-01 23:58:54 -0700

    104 is a piece of shit

---
+++ java/easy/104_maximum-depth-of-binary-tree.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//recursion: O(n), memory: O(n)
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        
+        int left = maxDepth(root.left); 
+        int right = maxDepth(root.right);
+        return max(left, right) + 1;
+    }
+    private int maxDepth_iterative(TreeNode root) {
+    if(root == null) {
+        return 0;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    int count = 0;
+    while(!queue.isEmpty()) {
+        int size = queue.size();
+        while(size > 0) {
+            TreeNode node = queue.poll();
+            size--;
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        count++;
+    }
+    return count;
+}
+
+}
+
+//edge cases
+/*
+Root could be equal to 0
+Will be slow O(n) time, with n being the number of tree nodes that exist
+If there is only one value on the branch, return 1
+Checks if there is only one branch straight down
+notes
+Recursive might be the best way to go
+DFS might help a lot
+
+*/
