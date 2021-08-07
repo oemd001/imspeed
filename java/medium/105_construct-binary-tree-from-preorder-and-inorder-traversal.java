d88ea8c3d01ffb7c8dd5c1eaf84ee8afb1bd28a0
oemd001 <kennethchen2001@gmail.com>
2021-08-06 23:33:50 -0700

    added the memory intensive method. Isn't super clear though

--- java/medium/105_construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ java/medium/105_construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -28,3 +28,37 @@ class Solution {
     }
 }
 
+/*
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+        //first 0: where is the root node
+        //second argument (0) and third arguemnt (inorder.length - 1) are the boundaries of the inorder array
+        //fourth and fifth argument basically have the preorder and inorder arrays
+    }
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        //if the starting position is greater than the length of the preorder array, return null
+        //if the initial position of the Inorder array is greater than the total length of the inorder array, return null
+        if (preStart > preorder.length - 1 || inStart > inEnd) return null;
+        
+        TreeNode root = new TreeNode(preorder[preStart]);
+        
+        //to add the left and right elements, we will need "inorder" because everything 
+        //right of the root (3, in this case) will be on the right of the tree
+        //everything left of the root, will be on the left
+        
+        int inIndex = 0;
+        
+        for (int i = inStart; i < inEnd; i++) {
+            if (root.val == inorder[i]) {
+                inIndex = i;
+            }
+        }
+        
+        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        
+        return root; 
+    }
+}
+*/
