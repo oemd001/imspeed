6e8fb588223c436d2741d37a1160bdc5908ac511
oemd001 <kennethchen2001@gmail.com>
2021-08-10 17:17:29 -0700

    added comments to 53

--- java/medium/56_merge-intervals.java
+++ java/medium/56_merge-intervals.java
@@ -2,25 +2,29 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         if (intervals.length == 1) return intervals; 
         
-        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0])); //heck you can sort 2D arrays with this?? memorize this mate
         
-        List<int[]> result = new ArrayList();
+        List<int[]> result = new ArrayList(); //because we don't know how many items are are sorting, we need to make this dynamically changing. hence, the "arraylist"
         
         int[] current_interval = intervals[0];
         result.add(current_interval);
         
-        for (int[] interval : intervals) {
-            int current_begin = current_interval[0];
-            int current_end = current_interval[1];
-            int next_begin = interval[0];
-            int next_end = interval[1];
+        for (int[] temp : intervals) { //this basically iterates through the entire array
+            int current_begin = current_interval[0]; //this sets the "begin" value [i][0]
+            int current_end = current_interval[1]; //this sets the "end" value [i][1]
+            int next_begin = temp[0]; //this sets the next "begin" value [i + 1][0]
+            int next_end = temp[1]; //this sets the next "end" value [i + 1][1]
             
-            if (current_end >= next_begin) {
-                current_interval[1] = Math.max(current_end, next_end);
+            if (current_end >= next_begin) { //this checks if the the current "end" value is greater than the next "begin" value
+                current_interval[1] = Math.max(current_end, next_end); //if so, set the current interval to the "next end" interval
+                //it's important to note that this changes it in the "output" array. [1,3] --> [1,6] (see example 1)
             }
             else {
-                current_interval = interval;
-                result.add(current_interval);
+                current_interval = temp; //if the next value is not greater than the current value
+                //value [i][0] is not greater than [i + 1][0], just add the next value
+                //however, if we get to the problem of [1,10][11,23][13,14], we can simply run the first
+                //if statement and the entire process would start again. 
+                result.add(current_interval); //apparently you can change the output array but modifying the current_interval variable
             }
         }
         
@@ -93,4 +97,3 @@ intervals[i] will be 2
 0 to 10^4
 
 */
-
