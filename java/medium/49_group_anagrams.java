b52efb8f742c3f54c266a59835469ca5dff82c94
oemd001 <kennethchen2001@gmail.com>
2021-08-10 17:06:08 -0700

    a bit easier to read

--- java/medium/49_group_anagrams.java
+++ java/medium/49_group_anagrams.java
@@ -1,59 +1,95 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        //This has to be an hashmap
-        //List of volues, with the same key, that is considered to be an anagram
-        //string array (strs) --> char, and sort the values alphabetically. 
-        //if the compared values are NOT the same, add another key
-        //time complexity  O(nmlogm): n is the largest size of words we can recieve and m is the largest length of the word we can receive
-        List<List<String>> same = new ArrayList<>();
-        HashMap<String, List<String>> map = new HashMap<>();
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 1) return intervals; 
         
-        for (String current : strs) {
-            char[] characters = current.toCharArray();
-            Arrays.sort(characters);
-            String sorted = new String(characters);
-            if (!map.containsKey(sorted)) {
-                map.put(sorted, new ArrayList<>());
-            }
+        Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
+        
+        List<int[]> result = new ArrayList();
+        
+        int[] current_interval = intervals[0];
+        result.add(current_interval);
+        
+        for (int[] temp : intervals) {
+            int current_begin = current_interval[0];
+            int current_end = current_interval[1];
+            int next_begin = temp[0];
+            int next_end = temp[1];
             
-            map.get(sorted).add(current);
+            if (current_end >= next_begin) {
+                current_interval[1] = Math.max(current_end, next_end);
+            }
+            else {
+                current_interval = temp;
+                result.add(current_interval);
+            }
         }
-        same.addAll(map.values());
-        return same;
+        
+        
+        return result.toArray(new int[result.size()][]);
     }
 }
 
 
 
-/* <--worked out solution
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        //eat --> tea, ate
-        //list of unique characters
-        List<List<String>> groupedAnagram = new ArrayList<>();
-        HashMap<String,List<String>> map = new HashMap<>();
-        
-        for(String current : strs) {
-            char[] characters = current.toCharArray();
-            Arrays.sort(characters);
-            String sorted = new String(characters);
-            
-            if (!map.containsKey(sorted)) {
-                map.put(sorted, new ArrayList<>());
-            }
-            
-            map.get(sorted).add(current);
-        }
-        groupedAnagram.addAll(map.values());
-        return groupedAnagram;
-    }
-}
-*/
-//constraints
+
 /*
-Edge Cases
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        //[start][end]
+        //so we need to check whether the "end" value is less than the value of start + 1
+        //if the value of [end] is greater than or equal to start, then we need to combine 
+        //start and end + 1 in to one array. 
+        
+        //is it safe to assume that for every x value, there will be an x value
+        
+        //one variable that holds on to the start value, one variable that holds on to the end value
+        //start value is held to basically prepare for hyptohetical merge
+        //end value is held to basically compare start + 1 value
+        //end + 1 value is held to also prepare for hypothetical merge. 
+        int j = 0;
+        int count = 0; 
+        int temp = 0; 
+        int start = intervals[j][0]; 
+        int end = intervals[j][1];  
+        int[][] result = new int[intervals.length][2];
+        
+        for (int i = 1; i < intervals.length; i++) {
+            temp = i;
+            start = intervals[j][0]; 
+            end = intervals[j][1];  
+            if (end >= intervals[i][0] && !(end >= intervals[i][1])) {
+                result[count][0] = start;
+                result[count][1] = intervals[i][1]; 
+                count++;
+            }
+            else if(end >= intervals[i][0] && end >= intervals[i][1]) {
+                while (end > intervals[i][1]) {
+                    if (end <= intervals[i][0]) result[count][0] = intervals[i][0];
+                    if (end <= intervals[i][1]) result[count][0] = intervals[i][1];
+                    i++;
+                    temp = i;
+                }
+            }
+            else {
+                result[count][0] = intervals[i][0];
+                result[count][1] = intervals[i][1]; 
+                count++;
+            }
+            i = temp; 
+            j++;
+        }
+        return result;    
+    }
+}
+/*
+[1,3]
+[2,6]
+[8,10]
+[15,18]
 
-Notes
-Length of strs: 1 - 10;
-How many items can I expect from the string?: 0 - 100;
+algo is probably floyd's tortoise and hare
+range of intervals in x axis will be from 1 to 10^4
+intervals[i] will be 2
+0 to 10^4
+
 */
