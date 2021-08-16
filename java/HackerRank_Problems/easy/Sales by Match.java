757e2de8c672a4cc8e8fe1b1a2861c91cd6fadce
oemd001 <kennethchen2001@gmail.com>
2021-08-15 17:56:27 -0700

    Made some changes to file hirearchy to diff between LC and HR. First HR problem used map, which is very important

---
+++ java/HackerRank_Problems/easy/Sales by Match.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+    // Write your code here
+    return helper(n, ar);
+    }
+    public static int helper(int n, List<Integer> arr) {
+        if (arr.size() == 0) return 0;
+        
+        Set<Integer> set = new HashSet();
+        int num_pairs = 0; 
+        
+        for (int i = 0; i < n; i++) {
+            if (!set.contains(arr.get(i))) {
+                set.add(arr.get(i));
+            }
+            else {
+                num_pairs++;
+                set.remove(arr.get(i));
+            }
+        }
+        return num_pairs; 
+        
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
