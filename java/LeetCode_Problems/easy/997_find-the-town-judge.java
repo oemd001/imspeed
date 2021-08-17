372bb0591e5f8396e378470c12404bfa342aaa1d
oemd001 <kennethchen2001@gmail.com>
2021-08-17 16:05:57 -0700

    Added comments

--- java/LeetCode_Problems/easy/997_find-the-town-judge.java
+++ java/LeetCode_Problems/easy/997_find-the-town-judge.java
@@ -9,8 +9,8 @@ class Solution {
         int[] count = new int[n + 1];
         
         for (int[] t : trust) {
-            count[t[0]]--;
-            count[t[1]]++;
+            count[t[0]]--; //given that position a trusts, we decrement
+            count[t[1]]++; //given that position b is being trusted, we increment
         }
         
         for (int i = 1; i <= n; i++) {
