c0e62de3fc7bf4d86b972726fe7c6882b7face2b
Kenneth Chen <kennethchen2001@gmail.com>
2021-08-27 17:35:11 -0700

    included my code part, I get it now!

--- java/LeetCode_Problems/medium/6_zigzag-conversion.java
+++ java/LeetCode_Problems/medium/6_zigzag-conversion.java
@@ -1,6 +1,6 @@
 class Solution {
     public String convert(String s, int numRows) {
-        return helper(s, numRows);
+        return myself(s, numRows);
     }
     
     private String helper(String s, int numRows) {
@@ -20,12 +20,10 @@ class Solution {
         We can see that there is a common theme here: 2numRows - 2 (2*4 - 2 = 6 steps, 2*3 -2 = 4 steps)
         Another theme we'll need to identify: the letters in between
             It's the similar, we still complete a cycle, but we will need to backtrack x amount back
-            Basically, what we do this is complete one cycle. After we complete one cycle, we backtrack back to 
-account for the value in the middle
+            Basically, what we do this is complete one cycle. After we complete one cycle, we backtrack back to account for the value in the middle
             e.g.numRows = 3: on row 2, we have 1 (A)
                 we add 4. We get our "L". However, we backtrack by 2 to get our "P"
-                This formula we can use to find our backtrack values is: n - 2i (where n is our number of steps 
-and i is the interval where the backtracking begins)
+                This formula we can use to find our backtrack values is: n - 2i (where n is our number of steps and i is the interval where the backtracking begins)
         */
         
         if (numRows == 1) return s;
@@ -40,12 +38,33 @@ and i is the interval where the backtracking begins)
             for (int j = i; j < length; j += n) {
                 c[count] = s.charAt(j); //this part literally gives us row 1 (the vertical areas)
                 count++;
-                if (stp > 0 && stp < n && j + stp < length) { //this part gives us the middle characters. 
-remember, the range must be within 0 to n
-                    c[count] = s.charAt(j + stp);             //in addition, the stp > 0 and stp < n part just 
-ensures that we are not interferring with the "column areas" when we want to add the middle values
+                if (stp > 0 && stp < n && j + stp < length) { //this part gives us the middle characters. remember, the range must be within 0 to n
+                    c[count] = s.charAt(j + stp);             //in addition, the stp > 0 and stp < n part just ensures that we are not interferring with the "column areas" when we want to add the middle values
                     count++;
                 }
+            }
+        }
+        return new String(c);
+    }
+    
+    private String myself(String s, int numRows) {
+        int n = 2 * numRows - 2; 
+        int length = s.length(); 
+        
+        if (numRows == 1) return s; 
+        
+        int pos = 0;
+        char[] c = new char[length];
+        
+        for (int i = 0; i < numRows; i++) {
+            int mid = n - 2 * i;
+            for (int j = i; j < length; j += n) {
+                c[pos] = s.charAt(j);
+                pos++;
+                if (mid > 0 && mid < n && mid + j < length) {
+                    c[pos] = s.charAt(j + mid);
+                    pos++;
+                } 
             }
         }
         return new String(c);
