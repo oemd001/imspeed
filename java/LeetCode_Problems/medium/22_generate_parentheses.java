ebbb7bbf55f131153cb72aee901816e9e00352a6
Kenneth Chen <kennethchen2001@gmail.com>
2021-09-10 23:46:44 -0700

    need to refresh backtracking, will attempt again

---
+++ java/LeetCode_Problems/medium/22_generate_parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            cur.append("(");
+            backtrack(ans, cur, open+1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(")");
+            backtrack(ans, cur, open, close+1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
+//Some things to consider:
+/*
+There will always be the () combination 
+There will always be a ()() combination
+
+For everything in between:
+(()()), (())(), ()(()) +3
+
+*/
