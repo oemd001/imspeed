8b584d8d85f121274bb150b5e3878b900814f279
oemd001 <kennethchen2001@gmail.com>
2022-06-07 22:10:17 -0700

    Added comments to 236

--- java/LeetCode_Problems/easy/1_twoSum.java
+++ java/LeetCode_Problems/easy/1_twoSum.java
@@ -1,32 +1,33 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        if (nums.length == 2) {
-            return new int[] {0, 1};
-        }
-        
+        return optimal(nums, target); 
+        /*
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] + nums[i] == target) {
-                    return new int[] {j, i};
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    return new int[] {i, j};
                 }
             }
         }
-        return new int[0];
+        return new int[] {0, 0};
+        */
     }
-}
-
-/*
-class Solution {
     
-    public int[] twoSum(int[] nums, int target) {
-         if(nums.length == 2) return new int[]{0,1};
+    public int[] optimal (int[] nums, int target) {
+        //complement, position
+        Map<Integer, Integer> map = new HashMap<>(); 
         
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] + nums[j - i] == target) return new int[] {j, j - i};
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement) && i != map.get(complement)) {
+                return new int[] {i, map.get(complement)};
             }
         }
-        return new int[] {0, 0};
+        return null; 
     }
 }
-*/
