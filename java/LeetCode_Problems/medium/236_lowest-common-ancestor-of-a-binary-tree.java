ff41107d79adfe4c2bdc6bf9e97aa9b0dfd4a1ef
oemd001 <kennethchen2001@gmail.com>
2022-06-07 22:11:40 -0700

    Added comments to 236

--- java/LeetCode_Problems/medium/236_lowest-common-ancestor-of-a-binary-tree.java
+++ java/LeetCode_Problems/medium/236_lowest-common-ancestor-of-a-binary-tree.java
@@ -9,42 +9,52 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return optimize(root, p, q);
+        /*
+        What happens in specific cases?
+        dfs recursive: I found "p", what's next? We want to find q
+        Try storing some other state
+        
+        I did not think of the naive solution first
+        What if we store a list of ancestors first leading up to a node?
+        Return the leftmost item from the list
+        
+        inductive reasoning: if there is no least common ancestor in the left sub branch, it'll be in the right one
+        If you found p in the left and q in the right, you return the root
+
+        Break this down in to cases
+        What happens if we're in the same branch?
+
+        If I don't find p or q in the left, then they have to be in the right
+        If it's not in the right, it's in the left
+
+        5 and 1. If right is non null and right is non null, I found p in right/left, q in the right/left
+        
+        inductive reasoning: if there is no least common ancestor in the left sub branch, it'll be in the right one
+    If you found p in the left and q in the right, you return the root
+
+    Break this down in to cases
+    What happens if we're in the same branch?
+
+    If I don't find p or q in the left, then they have to be in the right
+    If it's not in the right, it's in the left
+
+    5 and 1. If right is non null and right is non null, I found p in right/left, q in the right/left
+        */
+        return helper(root, p, q);
     }
     
+    
     public TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null; 
+        if (root == p || root == q || root == null) {
+            return root; 
+        }
         
         TreeNode left = helper(root.left, p, q);
         TreeNode right = helper(root.right, p, q);
         
-        if (root == p && (left == q || right == q))  return root; 
-        if (root == q && (left == p || right == p)) return root; 
-        
-        if (left == p && right == q) return root; 
-        if (left == q && right == p) return root; 
-        
-        if (left != null) return left; 
-        if (right != null) return right; 
-        
-        if (root == p) return p; 
-        if (root == q) return q; 
-        
-        return null;
-    }
-    
-    public TreeNode optimize(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root; 
-        // If p & q are not found, just return null
-		// If one of them is found, return that one to keep its record in the stack
-        TreeNode left = optimize(root.left, p, q);
-        TreeNode right = optimize(root.right, p, q); 
-        // If both of them are found, return the current node
         if (left == null) return right; 
         if (right == null) return left; 
         
-        return root;
+        return root; 
     }
-    
-    
 }
