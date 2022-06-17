64a8af583af42d72e081116313adfac362aa48b5
oemd001 <kennethchen2001@gmail.com>
2022-06-16 17:31:20 -0700

    Added bin search and better two ptr

--- java/LeetCode_Problems/medium/16_3sum-closest.java
+++ java/LeetCode_Problems/medium/16_3sum-closest.java
@@ -1,34 +1,85 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        //optimum solution is O(n^2)
-        Arrays.sort(nums);
+        /*
+        Approach 1: 
+        [5, 5, 5, 5] target = 1
+        [-1,2,1,-4]
         
-        //looping through, and adding two numbers at the same time. We are comparing three numbers. 
-        //if the sum - target is the least amount of value, then we will return that value, or update/return that sum. 
+        [-4, -1, 1, 2] target = 1
+        -4 + 2 = 2
+        -1 + 2 = 1
+        -1 + 1 = 0 ==> -1, 1, 2
         
-        int result = nums[0] + nums[1] + nums[nums.length - 1];
-        Arrays.sort(nums);
         
-        for (int i = 0; i < nums.length - 2; i++) {
-            int a_pointer = i + 1; 
-            int b_pointer = nums.length -1; 
-            
-            while (a_pointer < b_pointer) {
-                int current_sum = nums[i] + nums[a_pointer] + nums[b_pointer];
-                if (current_sum > target) {
-                    b_pointer--;
-                    //this means that the b_pointer is too large, we will want to decrement the value
-                }
-                else {
-                    a_pointer++;
-                    //if it's too small, we want to increase the value of the a_pointer–increase the value. 
-                }
-                if (Math.abs(current_sum - target) < Math.abs(result - target)) {
-                    result = current_sum; 
-                }
-            }
-        }
-        return result; 
+            minimum distance between target and the two sum approach to find nearest value
+            Once I find the closest value, I basically go through the array and find a value that's
+                equal, or has the most minimum distance between target and the array index
+                
+        [0,0,0] target ==> 1
+            return 0; 
+        */
+        //return two_pointer(nums, target); 
+        return binary_search(nums, target); //that's actually more optimized
         
     }
-}
+    
+    private int two_pointer(int[] nums, int target) {
+        if (nums.length == 3) {
+            int temp = 0; 
+            for (int i = 0; i < nums.length; i++) {
+                temp += nums[i];
+            }
+            return temp;             
+        }
+        
+        Arrays.sort(nums); //ascending order
+        
+        int left = 0; 
+        int right = nums.length - 1; 
+        int min = Integer.MAX_VALUE; 
+        int leftmost = 0; 
+        int rightmost = 0;
+        int final_value = 0; 
+        
+        while (left < right) {
+            int temp_value = nums[left] + nums[right];
+            leftmost = nums[left];
+            rightmost = nums[right];
+            if (temp_value > target) {
+                right--; 
+            }
+            else {
+                left++; 
+            }
+        }
+        
+        target = target - (leftmost + rightmost);
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (min > Math.abs(target - nums[i])) {
+                final_value = nums[i];
+                min = Math.min(min, Math.abs(target - nums[i]));
+            }
+        }
+        
+        return leftmost + rightmost + final_value;
+    }
+    
+    public int binary_search(int[] nums, int target) {
+        int diff = Integer.MAX_VALUE;
+        int sz = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < sz && diff != 0; ++i) {
+            for (int j = i + 1; j < sz - 1; ++j) {
+                int complement = target - nums[i] - nums[j];
+                var idx = Arrays.binarySearch(nums, j + 1, sz - 1, complement);
+                int hi = idx >= 0 ? idx : ~idx, lo = hi - 1;
+                if (hi < sz && Math.abs(complement - nums[hi]) < Math.abs(diff))
+                    diff = complement - nums[hi];
+                if (lo > j && Math.abs(complement - nums[lo]) < Math.abs(diff))
+                    diff = complement - nums[lo];
+            }
+        }
+        return target - diff;
+    }
+} 
