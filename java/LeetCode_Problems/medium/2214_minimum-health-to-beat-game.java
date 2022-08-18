9929b63c44543a34f50e6dc45d4a5e0edf01746b
Kenneth Chen <kennethchen2001@gmail.com>
2022-08-18 11:41:56 -0700

    Spent too much time on this prob

---
+++ java/LeetCode_Problems/medium/2214_minimum-health-to-beat-game.java
@@ -0,0 +1,18 @@
+ass Solution {
+	    public long minimumHealth(int[] damage, int armor) {
+		            
+		            long health = 1; 
+			            int max = 0; 
+				            
+				            for (int i = 0; i < damage.length; i++) {
+						                health += damage[i];
+								            max = Math.max(max, damage[i]);
+									                
+									                
+									            }
+					            
+					            health -= Math.min(max, armor);
+						            return health; 
+							        }
+}
+
