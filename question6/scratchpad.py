619c5626b7997fc7ce41749a8a93e092c1b13bdc
Kenneth Chen <kennethchen2001@gmail.com>
2024-07-17 13:20:12 -0400

    Update scratchpad.py

--- question6/scratchpad.py
+++ question6/scratchpad.py
@@ -1,24 +1,34 @@
 from PIL import Image
 import os
+import numpy as np
 
 directory_path = 'mnist_images'
 
-images = []
-for filename in os.listdir(directory_path):
-    if filename.endswith(".png"):
-        img_path = os.path.join(directory_path, filename)
-        images.append(Image.open(img_path))
+# Function to filter filenames
+def filter_filenames(filenames, suffixes):
+    return [filename for filename in filenames if any(filename.endswith(suffix + ".png") for suffix in suffixes)]
 
-## Pre-proc image
+# Get the list of filtered filenames
+suffixes = ["_8", "_9"]
+filtered_filenames = filter_filenames(os.listdir(directory_path), suffixes)
+
+# Load images
+images = []
+for filename in filtered_filenames:
+    img_path = os.path.join(directory_path, filename)
+    images.append(Image.open(img_path))
+
+# Pre-process images
 preprocessed_images = [img.convert('L').resize((28, 28)) for img in images]
 
-## Impl
-import numpy as np
-
+# Convert to numpy arrays and normalize
 image_arrays = np.array([np.array(img) for img in preprocessed_images])
 image_arrays = image_arrays.reshape(-1, 28, 28, 1) / 255.0  
 
-## Classify
+# Assume 'model' is your trained model
 predictions = model.predict(image_arrays)
 predicted_digits = np.argmax(predictions, axis=1)
-print(predicted_digits)
+
+# Print filenames with their corresponding predictions
+for filename, digit in zip(filtered_filenames, predicted_digits):
+    print(f"Filename: {filename}, Predicted Digit: {digit}")
