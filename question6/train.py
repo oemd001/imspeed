d0cad75ca15f5f9d92a97630edb0140def4bb9a7
Kenneth Chen <kennethchen2001@gmail.com>
2024-07-17 12:05:28 -0400

    Update train.py

--- question6/train.py
+++ question6/train.py
@@ -1,18 +1,21 @@
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
-from keras.utils import np_utils
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.utils import to_categorical
 
 # Load pre-shuffled MNIST data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Preprocess data
+# Reshape and normalize data
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32') / 255
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32') / 255
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
 
-# Build a simple CNN
+# Convert class vectors to binary class matrices
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Define model architecture
 model = Sequential([
     Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
     MaxPooling2D(pool_size=(2, 2)),
@@ -23,5 +26,8 @@ model = Sequential([
     Dense(10, activation='softmax')
 ])
 
+# Compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
 model.fit(x_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(x_test, y_test))
